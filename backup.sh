778caa15bf7d753aa967da92e301c355e03176d4
JustPin <68527260+Kepin-ui@users.noreply.github.com>
2021-08-11 05:33:09 -0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 date=$(date +%y-%m-%d)
-zip -r /root/BackupData-$date.zip /home/root/minecraft/*
+zip -r /root/BackupData-$date.zip /home/ubuntu/minecraft/*
 rclone copy /root/BackupData-$date.zip gdrive:Backup/
 rm -rf /root/BackupData-$date.zip
